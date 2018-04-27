cb7b58d006f877d1f0a90accb394e21a2d843688
Jose Antonio <j-osea-nt@hotmail.com>
2018-04-26 22:22:34 -0500

    add aniations in home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,32 @@
 import {Component, OnInit} from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 import { ListService } from '../services/lists.service';
 
+/* el estado void es cuando el componente no se encuentra dentro de la vista, esta detached (no está en la vista) */
+/* lo estados por defecto no se animan, se debe especificar las transiciones*/
+/* en angular hay nombres q estan definidos para la entrada y salida del elemento :enter, :leave*/
+/* la forma corta del enter es 'void => *', porque se se va desde void (fuera de la vista) a otro cualquier estado, esa es la entrada. Se puede utilizar :enter*/
+/* la forma corta de salida es es '* => void' */
+/* el * es un comodin para cualquier otro estado q no sea void */
+
 @Component({
   selector: 'app-home',
-  templateUrl:  'home.component.html'
+  templateUrl:  'home.component.html',
+  animations: [
+  	trigger('enterState',[
+  		state('void',style({
+  			transform: 'translateX(-100%)',
+  			opacity: 0
+  		})),
+  		transition(':enter',[
+  			animate(300,style({
+  				transform: 'translateX(0)',
+  				opacity: 1
+  			}))
+  		])
+  	])
+  ]
 })
 export class HomeComponent implements OnInit {
 
--- src/app/todos/creator/todos.creator.component.ts
+++ src/app/todos/creator/todos.creator.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { trigger,state,style,transition,animate } from '@angular/animations';
 
 import { TodoService } from '../../services/todos.service';
 import { ITodo, TStatus } from '../../structures/todos';
 
+
+/* en el state expanded, el height * es un comodin q indica q la altura del elemento sera la q requiera, tambien se puede especifcar una altura */
+/* en transition se define de donde a donde va ir la transicion (de estado a estado) para ejecutar la animacion*/
+/* */
 @Component({
 	selector: 'todo-creator',
-	templateUrl: 'todos.creator.component.html'
+	templateUrl: 'todos.creator.component.html',
+	animations: [
+		trigger('openClose',[
+			state('collapsed, void',style({height: '0px'})),
+			state('expanded',style({height: '*'})),
+			transition('collapsed <=> expanded',[animate(300,style({height: '*'})),animate(300)])
+		])
+	]
+	
 })
 export class TodoCreatorComponent implements OnInit {
 	
 	@Input() id : string;
+
+	public formState : string = 'collapsed';
 
 	public todo: ITodo = {content: '', status: TStatus.Created };
 
@@ -19,5 +34,17 @@ export class TodoCreatorComponent implements OnInit {
 
 	save(){
 		this.todoS.add(this.id,this.todo);
+	}
+
+	icon(){
+		return (this.formState == 'collapsed') ? 'fa-plus' : 'fa-caret-up';
+	}
+
+	label(){
+		return (this.formState == 'collapsed') ? 'Agregar nuevo pendiente' : 'Ocultar formulario';
+	}
+
+	toggleForm(){
+		this.formState = (this.formState == 'collapsed') ? 'expanded' : 'collapsed';
 	}
 }
