626a81a6adaf19b330c7ddb0951c95c36635d8a6
Jose Antonio <j-osea-nt@hotmail.com>
2018-05-16 21:38:59 -0500

    add animation to swipe

--- src/app/todos/card/todo.card.component.ts
+++ src/app/todos/card/todo.card.component.ts
@@ -1,13 +1,31 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { trigger,state,style,transition,animate } from '@angular/animations';
 
 import { ITodo } from '../../structures/todos';
 
 @Component({
 	selector: 'todo-card',
-	templateUrl: 'todo.card.component.html'
+	templateUrl: 'todo.card.component.html',
+	animations:[
+		trigger('pressAnimation',[
+			state('up,void',style({
+				transform: 'translateX(0)'
+			})),
+			state('down',style({
+				transform: 'translateX(-100px)'
+			})),
+			transition('up <=> down',[
+				animate(100,style({transform: 'translateX(0)'})),
+				animate(100)
+			])
+		])
+	]
 })
 export class TodoCardComponent implements OnInit{
 	@Input() todo : ITodo;
+	// down es con el click
+	// up no esta haciendo presion en el elemento
+	public press : string = 'up';
 
 	ngOnInit(){
 
