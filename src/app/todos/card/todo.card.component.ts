946ef386ce5eb19c4afd5ea0e0577708881518e5
Jose Antonio <j-osea-nt@hotmail.com>
2018-05-23 21:41:32 -0500

    event swipe and animations

--- src/app/todos/card/todo.card.component.ts
+++ src/app/todos/card/todo.card.component.ts
@@ -1,12 +1,31 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { trigger,state,style,transition,animate } from '@angular/animations';
 
-import { ITodo } from '../../structures/todos';
+import { ITodo, TStatus } from '../../structures/todos';
 
+// state 0 es el estado q representa a creado
+// state void representa q el elemento no está dentro de la vista
+// state 1 es el estado completo
+// se usa numeros pues esta q se usan Enums
+// en la primera animacion se pone el valor del estado y en la segunda animacion por lo general la duracion de la animacion
 @Component({
 	selector: 'todo-card',
 	templateUrl: 'todo.card.component.html',
 	animations:[
+		trigger('statusAnimation',[
+			state('0,void',style({
+				transform: 'translateX(0)',
+				opacity: 1
+			})),
+			state('1',style({
+				transform: 'translateX(-100%)',
+				opacity: 0
+			})),
+			transition('0 <=> 1',[
+				animate(200,style({transform: 'translateX(0)', opacity: 1})),
+				animate(200)
+			])
+		]),
 		trigger('pressAnimation',[
 			state('up,void',style({
 				transform: 'translateX(0)'
@@ -32,6 +51,6 @@ export class TodoCardComponent implements OnInit{
 	}
 
 	completed(){
-		console.log('hola')
+		this.todo.status = TStatus.Completed;
 	}
 }
